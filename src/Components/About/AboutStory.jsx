7bc1db6bf56edb40a3ad35007251bfb2455723b1
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-08-29 13:32:00 +0300

    fixes

--- src/Components/About/AboutStory.jsx
+++ src/Components/About/AboutStory.jsx
@@ -1,5 +1,5 @@
-import CategoryTag from "../Utility/Modules/CategoryTag"
-import "./about.css"
+import CategoryTag from "../Utility/Modules/CategoryTag";
+import "./about.css";
 
 function AboutStory() {
   return (
@@ -15,35 +15,37 @@ function AboutStory() {
             him to achieve his dream of becoming a lawyer. During a train ride,
             a moment of inspiration struck Vasily. He wished for a convenient
             study tool on his phone to help him prepare for the LSAT. However,
-            such an app didn&apos;t exist at the time. Determined to overcome this
-            hurdle, Vasily took matters into his own hands and developed one of
-            the earliest and most comprehensive LSAT apps on the market. The app
-            quickly gained popularity, becoming the #1 paid LSAT app for over a
-            year.
+            such an app didn&apos;t exist at the time. Determined to overcome
+            this hurdle, Vasily took matters into his own hands and developed
+            one of the earliest and most comprehensive LSAT apps on the market.
+            The app quickly gained popularity, becoming the #1 paid LSAT app for
+            over a year.
           </p>
         </div>
         <div className="story-img"></div>
       </div>
       <div className="story-second story-unit">
         <div className="story-img"></div>
-        <p>
-          This success prompted Vasily to establish FutureSphere, an app
-          development company with a strong focus on creating profitable and
-          impactful applications. Building on their initial achievement,
-          FutureSphere expanded their educational app portfolio to include exams
-          such as SAT, GMAT, MCAT, and more, catering to the needs of a broader
-          audience. As time went on, it became evident that the industry had
-          much more to offer than just exam-focused applications. People began
-          approaching FutureSphere, seeking guidance on how to develop their own
-          apps, what it takes to succeed, and how to navigate the complex world
-          of mobile applications. In response to this growing demand for
-          expertise and support, FutureSphere expanded its services and evolved
-          into a comprehensive resource for aspiring app creators - a company
-          known as FutureSphere.
-        </p>
+        <div className="story-text">
+          <p>
+            This success prompted Vasily to establish FutureSphere, an app
+            development company with a strong focus on creating profitable and
+            impactful applications. Building on their initial achievement,
+            FutureSphere expanded their educational app portfolio to include
+            exams such as SAT, GMAT, MCAT, and more, catering to the needs of a
+            broader audience. As time went on, it became evident that the
+            industry had much more to offer than just exam-focused applications.
+            People began approaching FutureSphere, seeking guidance on how to
+            develop their own apps, what it takes to succeed, and how to
+            navigate the complex world of mobile applications. In response to
+            this growing demand for expertise and support, FutureSphere expanded
+            its services and evolved into a comprehensive resource for aspiring
+            app creators - a company known as FutureSphere.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 
-export default AboutStory
+export default AboutStory;
