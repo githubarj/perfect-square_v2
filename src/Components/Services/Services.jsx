feb92feeec8377e07ab74cf23ffdb80353071eaf
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-02 21:34:44 +0300

    services prep

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,7 +1,17 @@
+import Navbar from "../Utility/Navbar/Navbar"
+import Footer from "../Utility/Footer/Footer"
+import "./services.css"
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className="services-container page">
+      <Navbar />
+      
+
+
+      <Footer />
+
+    </div>
   )
 }
 
---
+++ src/Components/Services/ServicesBody.jsx
@@ -0,0 +1,12 @@
+import "./services.css"
+
+function ServicesBody() {
+  return (
+    <div className="services-body-container">
+        
+
+    </div>
+  )
+}
+
+export default ServicesBody
