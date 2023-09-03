2eb06f6138c657c4dfee8ce3425dc67ffbf3759b
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-03 08:25:23 +0300

    services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,18 +1,17 @@
-import Navbar from "../Utility/Navbar/Navbar"
-import Footer from "../Utility/Footer/Footer"
-import "./services.css"
+import Navbar from "../Utility/Navbar/Navbar";
+import Footer from "../Utility/Footer/Footer";
+import "./services.css";
+import ServicesBody from "./ServicesBody";
 
 function Services() {
   return (
     <div className="services-container page">
       <Navbar />
-      
-
+      <ServicesBody />
 
       <Footer />
-
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
--- src/Components/Services/ServicesBody.jsx
+++ src/Components/Services/ServicesBody.jsx
@@ -1,12 +1,26 @@
-import "./services.css"
+import "./services.css";
+import { componentHeaders, services } from "./servicesData";
+import ComponentHeader from "../Utility/Modules/ComponentHeader";
+import Card from "../Utility/Card/Card";
 
 function ServicesBody() {
+  const header = componentHeaders[0];
+
+  const products = services.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
   return (
     <div className="services-body-container">
-        
-
+      <ComponentHeader
+        category={header.categoryTag}
+        h1={header.h1}
+        p={header.p}
+        span={header.span}
+      />
+      <div className="products-collection">{products}</div>
     </div>
-  )
+  );
 }
 
-export default ServicesBody
+export default ServicesBody;
