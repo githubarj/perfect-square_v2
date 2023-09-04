7b9b32822a4c22030768a54f008c1bb855d7cb9e
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-04 07:18:07 +0300

    article page

--- src/Components/Article/ArticleContent.jsx
+++ src/Components/Article/ArticleContent.jsx
@@ -1,18 +1,18 @@
-import "./article.css"
-import ArticleParagraph from "../Utility/Modules/ArticleParagraph"
+import "./article.css";
+import ArticleParagraph from "../Utility/Modules/ArticleParagraph";
+import { articleData } from "./articleData";
 
 function ArticleContent() {
+  const data = articleData.map((item) => {
+    return <ArticleParagraph key={item.id} {...item} />;
+  });
+
   return (
     <div className="a-c-container">
-        <div className="ac-slideshow">
-
-        </div>
-        <div className="ac-wrapper">
-            <ArticleParagraph />
-        </div>
-
+      <div className="ac-slideshow"></div>
+      <div className="ac-wrapper">{data}</div>
     </div>
-  )
+  );
 }
 
-export default ArticleContent
+export default ArticleContent;
--- src/Components/Utility/Modules/ArticleParagraph.jsx
+++ src/Components/Utility/Modules/ArticleParagraph.jsx
@@ -1,15 +1,22 @@
-import "./modules.css"
+import "./modules.css";
+import PropTypes from "prop-types";
 
-function ArticleParagraph() {
+function ArticleParagraph(props) {
   return (
     <div className="ap-container">
-        <h1>
-
-
-        </h1>
-        
+      <h1 className="ap-h1">{props.heading}</h1>
+      <ul>
+        {props.content.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default ArticleParagraph
+ArticleParagraph.propTypes = {
+  heading: PropTypes.string,
+  content: PropTypes.array,
+};
+
+export default ArticleParagraph;
