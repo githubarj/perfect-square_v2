2bac1b0a4e15a87997f5e073048007b27a9325fe
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-05 11:03:23 +0300

    fix

--- src/Components/Blogs/Collection.jsx
+++ src/Components/Blogs/Collection.jsx
@@ -6,13 +6,13 @@ function Collection() {
     <div className="collection-container">
       <div className="blog-groups">
         <p className="active">All</p>
-        <p>Solar Solutions</p>
         <p>Contruction & Project Management</p>
+        <p>Solar Solutions</p>
         <p>Borehole Drilling</p>
       </div>
       <div className="blog-collections">
         <BlogPanel />
-        <BlogPanel /> 
+        <BlogPanel />
         <BlogPanel />
         <BlogPanel />
         <BlogPanel />
