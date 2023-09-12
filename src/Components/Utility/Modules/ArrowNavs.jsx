a7cfdbb1aa366f724ce49f7ba4f95e2e150bc3c0
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-12 03:44:01 +0300

    colors

--- src/Components/Utility/Modules/ArrowNavs.jsx
+++ src/Components/Utility/Modules/ArrowNavs.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types"
 function ArrowNavs(props) {
   return (
     <div className="arrow-nav-btns">
-      <button className="nav-btn left" onClick={props.left} >
-        <img src="/images/left.png" alt="" />
+      <button className="nav-btn left" onClick={props.left}>
+        <img src="/images/left.png" alt="" className="arrow-size" />
       </button>
 
-      <button className="nav-btn right" onClick={props.right} >
-        <img src="/images/right.png" alt=""   />
+      <button className="nav-btn right" onClick={props.right}>
+        <img src="/images/right.png" alt="" className="arrow-size" />
       </button>
     </div>
   );
