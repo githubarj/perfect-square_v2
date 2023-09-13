4a276f04ac0e695a68dee6f447fda65b5872fa09
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-13 22:30:09 +0300

    done

--- src/Components/Utility/Modules/ComponentHeader.jsx
+++ src/Components/Utility/Modules/ComponentHeader.jsx
@@ -10,7 +10,7 @@ function ComponentHeader(props) {
         <h1 className="heading-XLarge">{props.h1}</h1>
         <p className="text-Medium ">
           {props.p}
-          <a href="mailto:info@oerfectsquare.cc">{props.span}</a>
+          <a href="mailto:info@perfectsquare.cc">{props.span}</a>
         </p>
       </div> 
     </div>
