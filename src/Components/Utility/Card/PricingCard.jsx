f71e5fcae923a5485319fd6cb69b688456ff94f5
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-15 10:29:37 +0300

    done

--- src/Components/Utility/Card/PricingCard.jsx
+++ src/Components/Utility/Card/PricingCard.jsx
@@ -1,39 +1,39 @@
-import "./card.css"
-import PropTypes from "prop-types"
+import "./card.css";
+import PropTypes from "prop-types";
 
 function PricingCard(props) {
-
-  const constantItems = props.constant.map ((item, index) => {
+  const constantItems = props.constant.map((item, index) => {
     return (
       <li key={index}>
         {item.text}
         <img src="/images/tick.png" alt="" />
       </li>
     );
-  }) 
+  });
 
   const uniqueItems = props.unique.map((item, index) => {
     return <li key={index}>{item.text}</li>;
   });
 
-
-
-  return ( 
+  return (
     <div className="pc-container">
       <h1 className="heading-Medium"> {props.product} </h1>
       <h1 className="heading-XLarge">KSH {props.price}</h1>
-      <button className="heading-XSmall">Get Started</button>
+      <a
+        href="https://api.whatsapp.com/send?phone=254722540649&text=Hello"
+        rel="noreferrer"
+        target="_blank"
+        className="heading-XSmall"
+      >
+        Get Started
+      </a>
       <div className="pc-details">
         <div className="pc-details-constant">
-          <ul>
-           {constantItems}
-          </ul>
+          <ul>{constantItems}</ul>
         </div>
         <div className="pc-details-unique">
           <p>Details</p>
-          <ul>
-            {uniqueItems}
-          </ul>
+          <ul>{uniqueItems}</ul>
         </div>
       </div>
     </div>
