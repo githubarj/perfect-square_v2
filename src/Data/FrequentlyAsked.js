baace1cc2da86da0cdad06a3621e2df49b959a3e
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-26 16:17:52 +0300

    done with faqs section

--- src/Data/FrequentlyAsked.js
+++ src/Data/FrequentlyAsked.js
@@ -1,33 +1,21 @@
 const faqs = [
   {
-    question: "How do you go about the drilling process?",
-    answer:
-      " Borehole drilling involves site survey, equipment setup, drilling casing installation, grouting, pump installation, and water quality testing to access clean groundwater. Borehole drilling involves site assessment, drilling rig setup, drilling, casing installation, well development, pump installation, water quality testing, and final documentation.",
+    question: "Where are you located?",
+    answer: `Lavington, Nairobi and  
+      Pioneer Building 2nd Floor Room 206, Oginga Odinga St KISUMU`,
   },
   {
-    question: "How do you go about the drilling process?",
-    answer:
-      " Borehole drilling involves site survey, equipment setup, drilling casing installation, grouting, pump installation, and water quality testing to access clean groundwater. Borehole drilling involves site assessment, drilling rig setup, drilling, casing installation, well development, pump installation, water quality testing, and final documentation.",
+    question: "What does your deal in?",
+    answer: `Our company offers SOLAR Power solutions from solar security & street lights, SOLAR back-up system, solar water pumps and solar water heaters.We offer water solutions from geological survey, borehole drilling process, water tower & tank installation and borehole equipping.
+Our company is a home of professionals who ensure successful project management and housing construction.`,
   },
   {
-    question: "How do you go about the drilling process?",
-    answer:
-      " Borehole drilling involves site survey, equipment setup, drilling casing installation, grouting, pump installation, and water quality testing to access clean groundwater. Borehole drilling involves site assessment, drilling rig setup, drilling, casing installation, well development, pump installation, water quality testing, and final documentation.",
+    question: "How can I get your products?",
+    answer: `For our products, refer to the catalogue and place an order via our email info@perfectsquare.cc`,
   },
   {
-    question: "How do you go about the drilling process?",
-    answer:
-      " Borehole drilling involves site survey, equipment setup, drilling casing installation, grouting, pump installation, and water quality testing to access clean groundwater. Borehole drilling involves site assessment, drilling rig setup, drilling, casing installation, well development, pump installation, water quality testing, and final documentation.",
-  },
-  {
-    question: "How do you go about the drilling process?",
-    answer:
-      " Borehole drilling involves site survey, equipment setup, drilling casing installation, grouting, pump installation, and water quality testing to access clean groundwater. Borehole drilling involves site assessment, drilling rig setup, drilling, casing installation, well development, pump installation, water quality testing, and final documentation.",
-  },
-  {
-    question: "How do you go about the drilling process?",
-    answer:
-      " Borehole drilling involves site survey, equipment setup, drilling casing installation, grouting, pump installation, and water quality testing to access clean groundwater. Borehole drilling involves site assessment, drilling rig setup, drilling, casing installation, well development, pump installation, water quality testing, and final documentation.",
+    question: "What is your area of operation?",
+    answer: `We offer our services and deliver our products countrywide.`,
   },
   {
     question: "How do you go about the drilling process?",
